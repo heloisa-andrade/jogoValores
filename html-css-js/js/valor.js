4b2717c5c712020932f810a716be16b8b7ff485b
heloisa-andrade <heloisa.andrade@picpay.com>
2025-01-17 11:34:17 -0300

    subindo um modal novo

--- html-css-js/js/valor.js
+++ html-css-js/js/valor.js
@@ -19,3 +19,15 @@ function selecionado(texto){
     'box-shadow: 0 0 0 5px rgba(0, 0, 255,0.5);';
      /* Simula a margem com uma sombra */
 }
+function showModal(){
+    var elemeny = document.getElementById("modal");
+    elemeny.classList.add("show-modal")
+}
+function hideModal(){
+    var elemeny = document.getElementById("modal");
+    elemeny.classList.remove("show-modal")
+}
+function imagem(){
+    var res = document.getElementById("res")
+    res.innerText = 'teste'
+}
